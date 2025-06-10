f34a5fc88c689ec2c10d62fe2c3afa8d19b5e72c
Rafael Camelo <rafaelcamelo31@gmail.com>
2025-06-09 22:51:06 -0400

    add new listOrders usecase to support grpc call

--- projects/clean_architecture/cmd/ordersystem/main.go
+++ projects/clean_architecture/cmd/ordersystem/main.go
@@ -43,6 +43,7 @@ func main() {
 	})
 
 	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+	listOrderUseCase := NewListOrderUseCase(db)
 
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
@@ -52,7 +53,7 @@ func main() {
 	go webserver.Start()
 
 	grpcServer := grpc.NewServer()
-	createOrderService := service.NewOrderService(*createOrderUseCase)
+	createOrderService := service.NewOrderService(*createOrderUseCase, *listOrderUseCase)
 	pb.RegisterOrderServiceServer(grpcServer, createOrderService)
 	reflection.Register(grpcServer)
 
--- projects/clean_architecture/internal/infra/grpc/service/order_service.go
+++ projects/clean_architecture/internal/infra/grpc/service/order_service.go
@@ -8,16 +8,19 @@ import (
 	"github.com/rafaelcamelo31/graduate-go-course/4-module/clean_architecture/internal/dto"
 	"github.com/rafaelcamelo31/graduate-go-course/4-module/clean_architecture/internal/infra/grpc/pb"
 	"github.com/rafaelcamelo31/graduate-go-course/4-module/clean_architecture/internal/usecase"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrdersUseCase  usecase.ListOrdersUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, listOrdersUseCase usecase.ListOrdersUseCase) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		ListOrdersUseCase:  listOrdersUseCase,
 	}
 }
 
@@ -38,4 +41,25 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 	}, nil
+}
+
+func (s *OrderService) GetOrders(context.Context, *emptypb.Empty) (*pb.Orders, error) {
+	output, err := s.ListOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	orders := &pb.Orders{}
+	for _, v := range output.Orders {
+		orders.Orders = append(orders.Orders,
+			&pb.Order{
+				Id:         fmt.Sprint(v.ID),
+				Price:      float32(v.Price),
+				Tax:        float32(v.Tax),
+				FinalPrice: float32(v.FinalPrice),
+			},
+		)
+	}
+
+	return orders, nil
 }
